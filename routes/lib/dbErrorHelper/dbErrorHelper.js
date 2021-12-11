c8f68c3bb9d08cc5fa99b1dcd483f06499bb28c7
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-12-11 08:24:47 -0500

    error update

---
+++ routes/lib/dbErrorHelper/dbErrorHelper.js
@@ -0,0 +1,61 @@
+function getUniqueErrorMessage(err) {
+    let output;
+
+    let message = err.message;
+
+    try {
+        let fieldName = message.substring(
+            message.lastIndexOf(".$") + 2,
+            message.lastIndexOf("_1")
+        );
+
+        let whereToSlice = fieldName.lastIndexOf(":") + 2;
+
+        output = fieldName.slice(whereToSlice) + " already exists";
+    } catch (e) {
+        output = "Unique field already exists";
+    }
+
+    return output;
+}
+
+function getErrorMessage(err) {
+    let errorObj = {};
+    let message = "";
+
+    if (err.code) {
+        switch (err.code) {
+            case 401:
+                errorObj.statusCode = err.code;
+                errorObj.message = err.message;
+                return errorObj;
+            case 404:
+                errorObj.statusCode = err.code;
+                errorObj.message = err.message;
+                return errorObj;
+            case 11000:
+            case 11001:
+                errorObj.statusCode = 409;
+                errorObj.message = getUniqueErrorMessage(err);
+                return errorObj;
+            default:
+                errorObj.statusCode = 500;
+                errorObj.message = "Something went wrong";
+                return errorObj;
+        }
+    } else if (err.message) {
+        errorObj.message = err.message;
+        errorObj.statusCode = 500;
+        return errorObj;
+    } else {
+        for (let errName in err.errors) {
+            if (err.errors[errName].message) {
+                message = err.errors[errName].message;
+            }
+        }
+
+        return message;
+    }
+}
+
+module.exports = getErrorMessage;
